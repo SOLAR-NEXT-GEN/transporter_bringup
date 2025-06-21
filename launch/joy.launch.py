7a27bda1bfc29dfce8d32b0dcfaa2b8688ba1f24
pboon09 <pboonchanachai@gmail.com>
2025-06-21 12:58:20 +0700

    Create joy.launch.py

---
+++ launch/joy.launch.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+
+
+def generate_launch_description():
+    # Joy node
+    joy_node = Node(
+        package='joy',
+        executable='joy_node',
+        name='joy_node',
+        output='screen',
+    )
+    
+    # Transporter joy node
+    transport_joy_node = Node(
+        package='transporter_joy',
+        executable='joy.py',
+        name='transporter_joy',
+        output='screen',
+    )
+    
+    # micro-ROS agent
+    micro_ros_agent = ExecuteProcess(
+        cmd=['ros2', 'run', 'micro_ros_agent', 'micro_ros_agent', 
+             'serial', '-b', '2000000', '--dev', '/dev/ttyACM0'],
+        output='screen',
+        emulate_tty=True,
+    )
+    
+    return LaunchDescription([
+        micro_ros_agent,
+        joy_node,
+        transport_joy_node,
+    ])
